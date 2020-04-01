ea392b3608715951aa8db9b7038464ab058a227f
Nate Sammons <nate.sammons@gmail.com>
2020-04-01 10:07:08 -0600

    Update script

--- update.sh
+++ update.sh
@@ -1,10 +1,18 @@
 cd $HOME
+
+echo "Updating homebrew"
 brew update
 brew upgrade
 brew cask upgrade
+
+echo "Updating OMZ, Powerlevel10K"
 git -C ~/.oh-my-zsh pull
 git -C ~/.oh-my-zsh/custom/themes/powerlevel10k pull
+
+echo "Updating vim plugins"
 git -C ~/.vim/bundle/Vundle.vim pull
+vim +PluginUpdate +qall
+
+echo "Updating dotfiles"
 git -C ~/.dotfiles pull
 zsh ~/.dotfiles/init.sh
-vim +PluginUpdate +qall
