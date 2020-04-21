62bef0650e85679ccf3ba2ec2c812bda5383dbcc
Nate Sammons <nate.sammons@gmail.com>
2020-04-20 21:29:06 -0600

    Run more init.sh

--- init.sh
+++ init.sh
@@ -25,3 +25,12 @@ ln -s ./.dotfiles/vimrc $HOME/.vimrc
 echo "Setup global gitignore"
 git config --global core.excludesfile ~/.dotfiles/global.gitignore
 
+
+## run init.sh in other dirs
+for dir in $HOME/.zsh.d $HOME/.zsh.d.$USER $HOME/.zsh.d.$(hostname -s); do
+	if [[ -f $dir/init.sh ]]; then
+		echo "Running init.sh in $dir"
+		( cd $dir; zsh ./init.sh )
+	fi
+done
+
