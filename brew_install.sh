98f71405aca8409e90f3ea94b1dd4d3933b9418e
Nate Sammons <nate.sammons@nasdaq.com>
2020-08-05 12:58:31 -0600

    Add fonts

--- brew_install.sh
+++ brew_install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+## install a bunch of brew packages
 cd `dirname $0`
 brew install `cat brew_packages.txt`
 
-brew tap homebrew/cask-fonts
-brew cask install font-firacode-nerd-font
-
 ## create a ~/.fzf.zsh file
 $(brew --prefix)/opt/fzf/install --no-update-rc --no-bash --all
+
+## install some nice fonts
+brew tap homebrew/cask-fonts
+brew cask install font-firacode-nerd-font
+brew cask install font-meslo-lg-nerd-font
 
